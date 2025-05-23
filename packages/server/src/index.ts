3f5928ff6dcea785e92dfc73a1578ef0b7d0a5f8
rkusan00 <renato.kusanovic@gmail.com>
2025-05-23 16:53:27 +0200

    Cleanup

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -61,10 +61,7 @@ export function onUserInteraction(
   }
   // Can have arbitrary return value (interpreted by target system)
   // FE is updated if updateDisplayState is true
-  return {
-    isCorrect,
-    updateDisplayState: true,
-  };
+  return { isCorrect, updateDisplayState: true };
 }
 
 export const hookMap = new Map(
